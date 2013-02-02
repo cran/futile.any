e8240f34c0f5c6d590a3bcb761b7c1a2584ae9fb
Brian Lee Yung Rowe <r@zatonovo.com>
2013-02-02 00:00:00 +0000

    version 1.3.0

--- R/any.R
+++ R/any.R
@@ -1,10 +1,12 @@
+# Gets the length of a vector or the rows of a matrix or data frame.
+anylength(data) %when% { ! is.null(nrow(data)) } %as% nrow(data)
+anylength(data) %as% length(data)
+
 # Get either names or colnames from a list or data.frame. This attempts to 
 # create some polymorphism around lists, vectors, and data.frames.
-anynames %when% (! is.null(names(data)))
-anynames %as% function(data) names(data)
-
-anynames %when% (! is.null(colnames(data)))
-anynames %as% function(data) colnames(data)
+anynames(data) %when% { ! is.null(names(data)) } %as% names(data)
+anynames(data) %when% { ! is.null(colnames(data)) } %as% colnames(data)
+anynames(data) %as% NULL
 
 "anynames<-" <- function(data, value)
 {
@@ -13,18 +15,34 @@ anynames %as% function(data) colnames(data)
   invisible(data)
 }
 
-# Gets the length of a vector or the rows of a matrix or data frame.
-anylength %when% (! is.null(nrow(data)))
-anylength %as% function(data) nrow(data)
-
-anylength %when% (TRUE)
-anylength %as% function(data) length(data)
-
 # Lists out the types of a data.frame or other object that supports anynames
-anytypes <- function(data, fun=class)
-{
+anytypes(data, fun=class) %when% {
+  is.null(dim(data))
+} %as% fun(data)
+anytypes(data, fun=class) %as% {
   ts <- apply(matrix(anynames(data), ncol=1), 1, function(x) fun(data[,x]))
   names(ts) <- anynames(data)
-
   return(ts)
 }
+
+# If an object is empty, it is defined but has length 0.
+is.empty(x) %::% a : logical
+is.empty(x) %as% { length(x) < 1 }
+
+is.bad(x) %::% a : logical
+is.bad(x) %when% { is.null(x) } %as% TRUE
+is.bad(x) %when% { is.empty(x) } %as% TRUE
+
+is.bad(x) %::% list : list
+is.bad(x) %as% { lapply(x, is.bad) }
+
+is.bad(x) %::% data.frame : matrix
+is.bad(x) %as% { sapply(x, is.bad) }
+
+is.bad(x) %::% matrix : matrix
+is.bad(x) %as% { apply(x,1, is.bad) }
+
+is.bad(x) %::% a : logical
+is.bad(x) %as% { is.na(x) }
+
+
